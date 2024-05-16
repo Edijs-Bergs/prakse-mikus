6f100192a7dce662e32b7b784f5bb4e141c4cba5
Mikus <mikus@mikus.com>
2024-05-16 14:03:44 +0300

    Noskatijos visu

--- plugins/mikus/movies/components/ActorForm.php
+++ plugins/mikus/movies/components/ActorForm.php
@@ -7,6 +7,7 @@ use Redirect;
 use ValidationException;
 use Mikus\Movies\Models\Actor;
 use Flash;
+use System\Models\File;
 
 class ActorForm extends ComponentBase
 {
@@ -19,15 +20,35 @@ class ActorForm extends ComponentBase
     }
 
 
-    public function onSave(){
+    public function onSubmit(){
+        $validator = Validator::make(
+            $form = Input::all(), [
+                'name' => 'required',
+                'lastname' => 'required'
+            ]
+        );
+
+        if ($validator->fails()){
+            throw new ValidationException($validator);
+        }
+
+
         $actor = new Actor();
         $actor->name = Input::get('name');
         $actor->lastname = Input::get('lastname');
         $actor->actorimage = Input::file('actorimage');
         $actor->save();
         Flash::success('Actor added!');
-        return Redirect::back();
+    }
 
+    public function onImageUpload() {
+        $image = Input::all();
+
+        $file = (new File())->fromPost($image['actorimage']);
+
+        return[
+            '#imageResult' => '<img src="' . $file->getThumb(200, 200, ['mode' => 'crop']) . '" >'
+        ];
     }
 
 }
